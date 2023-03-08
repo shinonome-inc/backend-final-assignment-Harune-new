77d1da2e406da33da09bceabfa48caf86a02cc0e
harune-pg <h.makino.22.6.533@4nonome.com>
2023-03-08 18:30:59 +0900

    不要な記述を削除

--- accounts/views.py
+++ accounts/views.py
@@ -29,6 +29,7 @@ class UserProfileView(LoginRequiredMixin, DetailView):
     slug_field = "username"
     slug_url_kwarg = "username"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        return context
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         return context
